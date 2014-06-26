45a32192e9bb28cec4af585de92bfbdbff8d228d
Ryan Phillips <ryan.phillips@rackspace.com>
2014-06-26 16:36:30 -0500

    change copyright to rackspace for now

--- source/conf.py
+++ source/conf.py
@@ -44,7 +44,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Virgo Agent Toolkit'
-copyright = u'2014, Virgo Agent Toolkit Authors'
+copyright = u'2014, Rackspace, US Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
